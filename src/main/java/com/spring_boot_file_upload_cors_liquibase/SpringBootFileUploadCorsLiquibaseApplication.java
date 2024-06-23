89ec49886d4a3268f7b77c8e014a5d7e99c4c852
saulemilioramirezcayoja <saulemilioramirezcayoja98@gmail.com>
2024-06-22 22:20:00 -0400

    first commit

---
+++ src/main/java/com/spring_boot_file_upload_cors_liquibase/SpringBootFileUploadCorsLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_file_upload_cors_liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileUploadCorsLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileUploadCorsLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot_file_upload_cors_liquibase/SpringBootFileUploadCorsLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_file_upload_cors_liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileUploadCorsLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
